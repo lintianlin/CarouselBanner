89febff65195c0b87ca214fa18b93f18a519aef4
lintianlin <mhjdxlwb@sina.com>
2017-04-02 15:09:19 +0800

    比较好用的轮播图和recyclerview的上拉加载、下拉刷新控

---
+++ RecyclerBanner/app/src/main/java/com/example/lintianlin/recyclerbanner/MainActivity.java
@@ -0,0 +1,307 @@
+package com.example.lintianlin.recyclerbanner;
+
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.ToxicBakery.viewpager.transforms.ABaseTransformer;
+import com.ToxicBakery.viewpager.transforms.DefaultTransformer;
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.listener.OnItemClickListener;
+import com.jcodecraeer.xrecyclerview.ProgressStyle;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, ViewPager.OnPageChangeListener, OnItemClickListener {
+
+    private XRecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private ArrayList<String> listData;
+    private int refreshTime = 0;
+    private int times = 0;
+    private ConvenientBanner header;
+    private ArrayList<Integer> localImages = new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView = (XRecyclerView) this.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        mRecyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
+        mRecyclerView.setArrowImageView(R.drawable.iconfont_downgrey);
+
+        header = (ConvenientBanner) LayoutInflater.from(this).inflate(R.layout.recyclerview_header, (ViewGroup) findViewById(android.R.id.content), false);
+        init();
+        mRecyclerView.addHeaderView(header);
+
+        mRecyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                refreshTime++;
+                times = 0;
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        listData.clear();
+                        for (int i = 0; i < 15; i++) {
+                            listData.add("item" + i + "after " + refreshTime + " times of refresh");
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        mRecyclerView.refreshComplete();
+                    }
+
+                }, 1000);            //refresh data here
+            }
+
+            @Override
+            public void onLoadMore() {
+                if (times < 2) {
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            for (int i = 0; i < 15; i++) {
+                                listData.add("item" + (1 + listData.size()));
+                            }
+                            mRecyclerView.loadMoreComplete();
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            for (int i = 0; i < 9; i++) {
+                                listData.add("item" + (1 + listData.size()));
+                            }
+                            mRecyclerView.setNoMore(true);
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                }
+                times++;
+            }
+        });
+
+        listData = new ArrayList<String>();
+        mAdapter = new MyAdapter(listData);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.refresh();
+
+
+    }
+
+
+    private void init() {
+        initImageLoader();
+        loadTestDatas();
+        //本地图片例子
+        header.setPages(
+                new CBViewHolderCreator<LocalImageHolderView>() {
+                    @Override
+                    public LocalImageHolderView createHolder() {
+                        return new LocalImageHolderView();
+                    }
+                }, localImages)
+                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+                //设置指示器的方向
+//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+//                .setOnPageChangeListener(this)//监听翻页事件
+                .setOnItemClickListener(this);
+
+//        convenientBanner.setManualPageable(false);//设置不能手动影响
+
+        //网络加载例子
+//        networkImages=Arrays.asList(images);
+//        convenientBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
+//            @Override
+//            public NetworkImageHolderView createHolder() {
+//                return new NetworkImageHolderView();
+//            }
+//        },networkImages);
+
+
+//手动New并且添加到ListView Header的例子
+//        ConvenientBanner mConvenientBanner = new ConvenientBanner(this,false);
+//        mConvenientBanner.setMinimumHeight(500);
+//        mConvenientBanner.setPages(
+//                new CBViewHolderCreator<LocalImageHolderView>() {
+//                    @Override
+//                    public LocalImageHolderView createHolder() {
+//                        return new LocalImageHolderView();
+//                    }
+//                }, localImages)
+//                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+//                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+//                        //设置指示器的方向
+//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+//                .setOnItemClickListener(this);
+//        listView.addHeaderView(mConvenientBanner);
+    }
+
+
+    //初始化网络图片缓存库
+    private void initImageLoader() {
+        //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
+                showImageForEmptyUri(R.drawable.ic_default_adimage)
+                .cacheInMemory(true).cacheOnDisk(true).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                getApplicationContext()).defaultDisplayImageOptions(defaultOptions)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
+        ImageLoader.getInstance().init(config);
+    }
+
+    /*
+    加入测试Views
+    * */
+    private void loadTestDatas() {
+        //本地图片集合
+        for (int position = 0; position < 7; position++)
+            localImages.add(getResId("ic_test_" + position, R.drawable.class));
+
+
+//        //各种翻页效果
+//        transformerList.add(DefaultTransformer.class.getSimpleName());
+//        transformerList.add(AccordionTransformer.class.getSimpleName());
+//        transformerList.add(BackgroundToForegroundTransformer.class.getSimpleName());
+//        transformerList.add(CubeInTransformer.class.getSimpleName());
+//        transformerList.add(CubeOutTransformer.class.getSimpleName());
+//        transformerList.add(DepthPageTransformer.class.getSimpleName());
+//        transformerList.add(FlipHorizontalTransformer.class.getSimpleName());
+//        transformerList.add(FlipVerticalTransformer.class.getSimpleName());
+//        transformerList.add(ForegroundToBackgroundTransformer.class.getSimpleName());
+//        transformerList.add(RotateDownTransformer.class.getSimpleName());
+//        transformerList.add(RotateUpTransformer.class.getSimpleName());
+//        transformerList.add(StackTransformer.class.getSimpleName());
+//        transformerList.add(ZoomInTransformer.class.getSimpleName());
+//        transformerList.add(ZoomOutTranformer.class.getSimpleName());
+//
+//        transformerArrayAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 通过文件名获取资源id 例子：getResId("icon", R.drawable.class);
+     *
+     * @param variableName
+     * @param c
+     * @return
+     */
+    public static int getResId(String variableName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(variableName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    // 开始自动翻页
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //开始自动翻页
+        header.startTurning(5000);
+    }
+
+    // 停止自动翻页
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //停止翻页
+        header.stopTurning();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Toast.makeText(this,"监听到翻到第"+position+"了",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+//        点击后加入两个内容
+//        localImages.clear();
+//        localImages.add(R.drawable.ic_test_2);
+//        localImages.add(R.drawable.ic_test_4);
+//        convenientBanner.notifyDataSetChanged();
+
+        //控制是否循环
+//        convenientBanner.setCanLoop(!convenientBanner.isCanLoop());
+
+
+        String transforemerName = DefaultTransformer.class.getSimpleName();
+        try {
+            Class cls = Class.forName("com.ToxicBakery.viewpager.transforms." + transforemerName);
+            ABaseTransformer transforemer = (ABaseTransformer) cls.newInstance();
+            header.getViewPager().setPageTransformer(true, transforemer);
+
+            //部分3D特效需要调整滑动速度
+            if (transforemerName.equals("StackTransformer")) {
+                header.setScrollDuration(1200);
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Toast.makeText(this, "点击了第" + position + "个", Toast.LENGTH_SHORT).show();
+    }
+}
